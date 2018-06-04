2b6d86d5b3ebca3d185aaab4aaa1fe100e515906
Elliot Jane Lader <elliot.j.lader@gmail.com>
2018-06-04 14:35:28 -0600

    Push ready to test local server

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const morgan = require("morgan");
+
 const app = express();
 
 const coffees = require("./routes/coffees");
 
-app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use("/coffees", coffees);
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-    const err = new Error("Not Found");
-    err.status = 404;
-    next(err);
-});
+app.use(notFound)
+app.use(errorHandler)
 
-// error handler
-app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: req.app.get("env") === "development" ? err.stack : {}
-    });
-});
+// eslint-disable-next-line
+function notFound(req, res, next) {
+    const url = req.originalUrl
+    if (!/favicon\.ico$/.test(url) && !/robots\.txt$/.test(url)) {
+        // Don't log less important (automatic) browser requests
+        console.error('[404: Requested file not found] ', url)
+    }
+    res.status(404).send({error: 'Url not found', status: 404, url})
+}
+
+// eslint-disable-next-line
+function errorHandler(err, req, res, next) {
+    console.error('ERROR', err)
+    const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
+    res.status(500).send({error: err.message, stack, url: req.originalUrl})
+}
 
 module.exports = app;
